6131bbe06be29a4568cf0e1e7c9ab620162a3bef
Steven Lin <stevenlin159@gmail.com>
2020-03-23 13:37:00 -0400

    Added Buttons and Textfield

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "react-bulma-components/dist/react-bulma-components.min.css";
+import { Button } from 'react-bulma-components/dist';
+
 
 // Import components
 import Room from "./components/Room";
 
 function Home() {
-  return <div>Welcome to Weiv</div>
+  return (
+      <div>Welcome to Weiv
+        <Button color="primary">Join Room</Button>
+        <Button color="primary">Create Room</Button>
+        <input class="input is-info" type="text" placeholder="Info input">
+        </input>
+        </div>
+
+    );
 }
 
 function App() {
